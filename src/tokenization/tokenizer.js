94d738945b311c0211a28b826c92a4264cda7e5a
ahmed ayoub <ahmed.mahmoud@zalando.de>
2018-05-27 19:19:07 +0200

    simplifying and improving tokenizer

--- src/tokenization/tokenizer.js
+++ src/tokenization/tokenizer.js
@@ -1,140 +1,40 @@
-export const tokenizer = function ( content ) {
+export const tokenizer = function ( content, includeMatches ) {
   let explainedPattern = [
+    '(?:[\\[\u200E])?',
+    '((?:\\d{1,4}[\\/\\-\\.]*){2,3})(?:[,\\s]+)',
+    '(?:.+?\\s)?((?:\\d{1,2}:?){2}\\d{1,2})',
+    '(?:[:\\-\\s\\]]+)',
+    '([aAPpMm:\\.\\s]+)?',
+    '[\\]\\-\\s]*',
+    '([^:]+):\\s?(.+)'
+  ].join( '' );
 
-    //might start with [
-    "(\\[?)",
+  let re = new RegExp( explainedPattern, 'g' );
 
-    /*
-    * matches two occurrences of (1-4 digits followed by single character (a dash '-' or slash '/' or a dot '.')
-    * representing day and month,  followed by 2-4 digits representing the year), this means it will match all the following:
-    *
-    * single day, single month, year at the end
-    * - 1.1.18
-    * - 1.1.218
-    * - 1.1.2018
-    *
-    * double day, single month, year at the end
-    * - 12.1.18
-    * - 12.1.218
-    * - 12.1.2018
-    *
-    * double day, double month, year at the end
-    * - 12.12.18
-    * - 12.12.218
-    * - 12.12.2018
-    *
-    * single day, single month, year at the beginning
-    * - 18.1.1
-    * - 218.1.1
-    * - 2018.1.1
-    *
-    * double day, single month, year at the beginning
-    * - 18.12.1
-    * - 218.12.1
-    * - 2018.12.1
-    *
-    * double day, double month, year at the beginning
-    * - 18.12.12
-    * - 218.12.12
-    * - 2018.12.12
-    *
-    * single day, single month, year at the end
-    * - 1-1-18
-    * - 1-1-218
-    * - 1-1-2018
-    *
-    * double day, single month, year at the end
-    * - 12-1-18
-    * - 12-1-218
-    * - 12-1-2018
-    *
-    * double day, double month, year at the end
-    * - 12-12-18
-    * - 12-12-218
-    * - 12-12-2018
-    *
-    * single day, single month, year at the beginning
-    * - 18-1-1
-    * - 218-1-1
-    * - 2018-1-1
-    *
-    * double day, single month, year at the beginning
-    * - 18-12-1
-    * - 218-12-1
-    * - 2018-12-1
-    *
-    * double day, double month, year at the beginning
-    * - 18-12-12
-    * - 218-12-12
-    * - 2018-12-12
-    *
-    * single day, single month, year at the end
-    * - 1/1/18
-    * - 1/1/218
-    * - 1/1/2018
-    *
-    * double day, single month, year at the end
-    * - 12/1/18
-    * - 12/1/218
-    * - 12/1/2018
-    *
-    * double day, double month, year at the end
-    * - 12/12/18
-    * - 12/12/218
-    * - 12/12/2018
-    *
-    * single day, single month, year at the beginning
-    * - 18/1/1
-    * - 218/1/1
-    * - 2018/1/1
-    *
-    * double day, single month, year at the beginning
-    * - 18/12/1
-    * - 218/12/1
-    * - 2018/12/1
-    *
-    * double day, double month, year at the beginning
-    * - 18/12/12
-    * - 218/12/12
-    * - 2018/12/12
-    * */
-    "((\\d{1,4}(\\-|\\/|\\.){1}){2}\\d{1,4})",
+  let statistics = {
+    dates   : [],
+    times   : [],
+    names   : [],
+    messages: []
+  };
 
-    /*
-    * separator between date and time with one occurrence for one of:
-    * - a space followed by 1-3 characters then another space
-    * - a comma followed by a space
-    * - any character followed by a space
-    * */
+  let matches = [];
 
-    "((\\s.{1,3}\\s|\\s)|,\\s|\\.\\s){1}",
+  let match;
 
-    // a group for date and time
-    "(",
+  while ( match = re.exec( content ) ) {
+    includeMatches && matches.push( match  );
 
-    /*
-    * match time
-    * match 1-2 digits followed by a colon ':' (hours), followed by two digits (minutes),
-    * followed by an optional (colon followed by two digits) which are the seconds
-    * */
-    "((\\d{1,2}\\:)\\d{2}(:\\d{2})?)",
+    let [ , date, time, , name, message ] = match;
 
-    /*
-    * day/night
-    * */
-    "(\\s(a|p)m?|\\s(A|P)?M|\\s(a|p)?\\.(\\s)?\\m.)?",
+    statistics.dates.push( date );
+    statistics.times.push( time );
+    statistics.names.push( name );
+    statistics.messages.push( message );
+  }
 
-    ")",
-
-    /*
-    * it might end with one of:
-    * - ']' followed by a space
-    * - a space followed by a dash '-' followed by another space
-    * - a colon ':'
-    * */
-    "(\\]\\s|\\s\\-\\s|\\:)"
-  ].join( "" );
-
-  let re = new RegExp( explainedPattern, "g" );
-  return re.exec( content )
-}
+  return {
+    matches,
+    statistics
+  }
+};
--- src/tokenization/tokenizer.test.js
+++ src/tokenization/tokenizer.test.js
@@ -1,20 +1,42 @@
 import { usecases } from './mocks';
 import { tokenizer } from './tokenizer';
+import simplifiedMessages from './mocks/whatsapp.txt';
 
 describe( 'tokenizer', () => {
 
   it( 'should cover all use cases', () => {
     usecases.forEach( ( [ usecase, expectedResult ] ) => {
-
+      let { matches } = tokenizer( usecase, true );
+      let [ result ]  = matches;
       if ( expectedResult ) {
-        //str.match returns array-like object which has more
+        //str.exec returns array-like object which has more
         //properties then the regular array (index, input, groups .. )
-        //this is why we wrapped it in Array.from
-        expect( Array.from( tokenizer( usecase ) ) ).toEqual( expectedResult );
+        //this is why we wrapped it in [...result]
+        expect( [ ...result ] ).toEqual( expectedResult );
       } else {
-        expect( tokenizer( usecase ) ).toBeFalsy();
+        expect( result ).toBeFalsy();
       }
     } );
   } );
 
+  it( 'simplified chat', () => {
+    let { statistics, matches } = tokenizer( simplifiedMessages, true );
+
+    expect( statistics ).toEqual( {
+      dates   : [ '6/13/16', '6/13/16', '6/13/16' ],
+      times   : [ '10:57', '2:48', '2:49' ],
+      names   : [ 'user1', 'user2', 'user2' ],
+      messages: [ 'Hi,','Hey user1, how are you?', 'how was your weekend? I hope it was sunny! 😍' ]
+    } );
+
+    //remove regex meta (index, length .. etc)
+    let pureMatches = matches.map(m=>[...m]);
+    expect(pureMatches).toEqual(
+      [
+        ['6/13/16, 10:57 AM - user1: Hi,', '6/13/16', '10:57', 'AM ', 'user1', 'Hi,'],
+      ['6/13/16, 2:48 PM - user2: Hey user1, how are you?', '6/13/16', '2:48', 'PM ', 'user2', 'Hey user1, how are you?'],
+        ['6/13/16, 2:49 PM - user2: how was your weekend? I hope it was sunny! 😍', '6/13/16', '2:49', 'PM ', 'user2', 'how was your weekend? I hope it was sunny! 😍']
+      ]
+    )
+  } );
 } );
